a953b43d8154e9c5b91cab5f72922bf6765bc724
fabervae <mesebii@hotmail.com>
2017-05-07 22:27:17 +0200

    Small renaming

--- src/DoubleWrapper.java
+++ src/DoubleWrapper.java
@@ -1,17 +1,17 @@
 public class DoubleWrapper<Arg1, Arg2> {
-    private Arg1 key;
-    private Arg2 tag;
+    private Arg1 arg1;
+    private Arg2 arg2;
     
-    public DoubleWrapper(Arg1 key, Arg2 tag) {
-        this.key = key;
-        this.tag = tag;
+    public DoubleWrapper(Arg1 arg1, Arg2 arg2) {
+        this.arg1 = arg1;
+        this.arg2 = arg2;
     }
     
-    public Arg2 getTag() {
-        return tag;
+    public Arg1 getArg1() {
+        return arg1;
     }
     
-    public Arg1 getKey() {
-        return key;
+    public Arg2 getArg2() {
+        return arg2;
     }
 }
