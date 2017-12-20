fa18861a767fcf334f5bf7f530c1a996227adbe6
fabervae <mesebii@hotmail.com>
2017-12-20 22:42:16 +0100

    Made the green color darker for readability

--- src/data/DataClass.java
+++ src/data/DataClass.java
@@ -2,9 +2,9 @@ package data;
 import java.awt.Color;
 
 public class DataClass {
-    public final static Color INFO_COLOR = Color.BLUE;
-    public final static Color NEW_MUSIC_COLOR = Color.GREEN;
-    public static final Color DEL_MUSIC_COLOR = Color.ORANGE;
-    public static final Color ERROR_COLOR = Color.RED;
+    public static final Color INFO_COLOR        = Color.BLUE;
+    public static final Color NEW_MUSIC_COLOR   = new Color(0, 220, 0);
+    public static final Color DEL_MUSIC_COLOR   = Color.ORANGE;
+    public static final Color ERROR_COLOR       = Color.RED;
 
 }
