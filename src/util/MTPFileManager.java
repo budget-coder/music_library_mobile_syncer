60177fb3e52a2c8d27aa7a12a3ea15ec04c0f9a8
fabervae <mesebii@hotmail.com>
2018-01-14 17:28:27 +0100

    From jmtpe. Changed single-character names for readability

--- src/util/MTPFileManager.java
+++ src/util/MTPFileManager.java
@@ -21,177 +21,174 @@ import jmtp.PortableDeviceToHostImpl32;
  *         Handles MTP device operation logic
  */
 public class MTPFileManager {
+    //boolean debug = false;
+    private PortableDevice device;
 
-	boolean debug = false;
+    //public synchronized void openDevice(PortableDevice portableDevice) {
+    public void openDevice(PortableDevice portableDevice) {
+        device = portableDevice;
+        device.open();
+    }
 
-	private PortableDevice device;
+    public void closeDevice() {
+        if (device != null) {
+            device.close();
+            device = null;
+        }
+    }
 
-	public PortableDevice getDevice() {
-		return device;
-	}
+    //public synchronized boolean isOpen() {
+    public boolean isOpen() {
+        if (device != null) {
+            return true;
+        }
+        return false;
+    }
+    
+    public PortableDevice getDevice() {
+        return device;
+    }
 
-	public synchronized void openDevice(PortableDevice portableDevice) {
-		device = portableDevice;
-		device.open();
-	}
+    public void addFile(File file, String mtpPath)
+            throws FileNotFoundException, IOException, COMException {
+        deleteFile(file.getName(), mtpPath);
+        String lastpartofpath = mtpPath
+                .substring(mtpPath.lastIndexOf("\\") + 1);
+        PortableDeviceStorageObject storage = getStorage();
+        PortableDeviceFolderObject folder = MTPUtil.createFolder(mtpPath,
+                storage, null, lastpartofpath);
 
-	public void closeDevice() {
-		if (device != null) {
-			device.close();
-			device = null;
-		}
-	}
+        // PortableDeviceAudioObject object = folder.addAudioObject(file, "--",
+        // "--", new BigInteger("0"));
+        folder.addAudioObject(file, "--", "--", new BigInteger("0"));
+        // LogUtil.debugPrint(LogUtil.LOG_LEVEL_LESS,
+        // this.getClass().getSimpleName(), "Copied " + file.getAbsolutePath() +
+        // " to " + mtpPath
+        // + " on " + getDevice().getModel());
+    }
 
-	public synchronized boolean isOpen() {
-		if (device != null) {
-			return true;
-		}
-		return false;
-	}
+    public ArrayList<PortableDeviceObject> getFiles(String path)
+            throws COMException {
+        String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
+        PortableDeviceStorageObject storage = getStorage();
+        PortableDeviceFolderObject folder = MTPUtil.createFolder(path, storage,
+                null, lastpartofpath);
 
-	public void addFile(File file, String mtpPath) throws FileNotFoundException, IOException, COMException {
-		deleteFile(file.getName(), mtpPath);
+        ArrayList<PortableDeviceObject> newFiles = new ArrayList<PortableDeviceObject>();
+        for (PortableDeviceObject object : folder.getChildObjects()) {
+            newFiles.add(object);
+        }
+        return newFiles;
 
-		String lastpartofpath = mtpPath.substring(mtpPath.lastIndexOf("\\") + 1);
-		PortableDeviceStorageObject storage = getStorage();
-		PortableDeviceFolderObject folder = MTPUtil.createFolder(mtpPath, storage, null, lastpartofpath);
+    }
 
-		//PortableDeviceAudioObject object = folder.addAudioObject(file, "--", "--", new BigInteger("0"));
-		folder.addAudioObject(file, "--", "--", new BigInteger("0"));
+    public ArrayList<PortableDeviceObject> getNewFiles(Date lastChecked,
+            String path) throws COMException {
+        String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
+        PortableDeviceStorageObject storage = getStorage();
+        PortableDeviceFolderObject folder = MTPUtil.createFolder(path, storage,
+                null, lastpartofpath);
 
-		//LogUtil.debugPrint(LogUtil.LOG_LEVEL_LESS, this.getClass().getSimpleName(), "Copied " + file.getAbsolutePath() + " to " + mtpPath
-		//		+ " on " + getDevice().getModel());
-	}
+        ArrayList<PortableDeviceObject> newFiles = new ArrayList<PortableDeviceObject>();
+        for (PortableDeviceObject object : folder.getChildObjects()) {
+            if (object.getDateModified() != null
+                    && object.getDateModified().after(lastChecked)) {
+                newFiles.add(object);
+            }
+        }
+        return newFiles;
 
-	public ArrayList<PortableDeviceObject> getFiles(String path) throws COMException {
-		String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
-		PortableDeviceStorageObject storage = getStorage();
-		PortableDeviceFolderObject folder = MTPUtil.createFolder(path, storage, null, lastpartofpath);
+    }
 
-		ArrayList<PortableDeviceObject> newFiles = new ArrayList<PortableDeviceObject>();
+    public PortableDeviceObject findFile(String name, String path)
+            throws COMException {
+        PortableDeviceStorageObject storage = getStorage();
+        if (storage == null) {
+            return null; // this is not a storage device
+        }
+        PortableDeviceFolderObject folder;
+        String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
+        if (!lastpartofpath.equals("")) {
+            folder = MTPUtil.createFolder(path, storage, null, lastpartofpath);
 
-		for (PortableDeviceObject object : folder.getChildObjects()) {
-			newFiles.add(object);
-		}
-		return newFiles;
+            for (PortableDeviceObject object : folder.getChildObjects()) {
+                if (object.getOriginalFileName().equals(name)) {
+                    return object;
+                }
+            }
+        } else {
+            for (PortableDeviceObject object : storage.getChildObjects()) {
+                if (object.getOriginalFileName().equals(name)) {
+                    return object;
+                }
+            }
+        }
+        return null;
+    }
 
-	}
+    public void getFile(String objectId, String destPath) throws COMException {
+        new PortableDeviceToHostImpl32().copyFromPortableDeviceToHost(objectId,
+                destPath, device);
+    }
 
-	public ArrayList<PortableDeviceObject> getNewFiles(Date lastChecked, String path) throws COMException {
-		String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
-		PortableDeviceStorageObject storage = getStorage();
-		PortableDeviceFolderObject folder = MTPUtil.createFolder(path, storage, null, lastpartofpath);
+    public boolean deleteFile(String name, String path) throws COMException {
+        PortableDeviceObject fileObject = findFile(name, path);
+        if (fileObject != null && fileObject.canDelete()) {
+            fileObject.delete();
+            return true;
+        }
+        return false;
+    }
 
-		ArrayList<PortableDeviceObject> newFiles = new ArrayList<PortableDeviceObject>();
+    public void deleteAllFiles(String path) throws COMException {
+        String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
+        PortableDeviceStorageObject storage = getStorage();
 
-		for (PortableDeviceObject object : folder.getChildObjects()) {
-			if (object.getDateModified()!=null && object.getDateModified().after(lastChecked)) {
-				newFiles.add(object);
-			}
-		}
-		return newFiles;
+        PortableDeviceFolderObject folder = MTPUtil.createFolder(path, storage,
+                null, lastpartofpath);
+        for (PortableDeviceObject fileObject : folder.getChildObjects()) {
+            if (fileObject != null && fileObject.canDelete()) {
+                fileObject.delete();
+            }
+        }
+    }
 
-	}
+    public void createFolder(String path) throws COMException {
+        String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
+        PortableDeviceStorageObject storage = getStorage();
+        MTPUtil.createFolder(path, storage, null, lastpartofpath);
+    }
 
-	public PortableDeviceObject findFile(String name, String path) throws COMException {
-		PortableDeviceStorageObject storage = getStorage();
-		PortableDeviceFolderObject folder;
+    public ArrayList<String> getAllFilesByName(String path) {
+        ArrayList<String> fileNames = new ArrayList<String>();
+        String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
+        PortableDeviceStorageObject storage = getStorage();
 
-		if (getStorage() == null) {
-			// this is not a storage device
-			return null;
-		}
+        PortableDeviceFolderObject folder = MTPUtil.createFolder(path, storage,
+                null, lastpartofpath);
+        for (PortableDeviceObject fileObject : folder.getChildObjects()) {
+            if (fileObject != null) {
+                fileNames.add(fileObject.getOriginalFileName());
+            }
+        }
+        return fileNames;
+    }
 
-		String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
+    public static PortableDevice[] getDevices() {
+        PortableDeviceManager manager = new PortableDeviceManager();
+        manager.refreshDeviceList();
+        return manager.getDevices();
+    }
 
-		if (!lastpartofpath.equals("")) {
-			folder = MTPUtil.createFolder(path, storage, null, lastpartofpath);
-
-			for (PortableDeviceObject object : folder.getChildObjects()) {
-				if (object.getOriginalFileName().equals(name)) {
-					return object;
-				}
-			}
-		} else {
-			for (PortableDeviceObject object : storage.getChildObjects()) {
-				if (object.getOriginalFileName().equals(name)) {
-					return object;
-				}
-			}
-		}
-
-		return null;
-	}
-
-	public void getFile(String objectId, String destPath) throws COMException {
-		new PortableDeviceToHostImpl32().copyFromPortableDeviceToHost(objectId, destPath, device);
-	}
-
-	public boolean deleteFile(String name, String path) throws COMException {
-		PortableDeviceObject fileObject = findFile(name, path);
-
-		if (fileObject != null && fileObject.canDelete()) {
-			fileObject.delete();
-			return true;
-		}
-		return false;
-	}
-
-	public void deleteAllFiles(String path) throws COMException {
-		String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
-		PortableDeviceStorageObject storage = getStorage();
-
-		PortableDeviceFolderObject folder = MTPUtil.createFolder(path, storage, null, lastpartofpath);
-
-		for (PortableDeviceObject fileObject : folder.getChildObjects()) {
-			if (fileObject != null && fileObject.canDelete()) {
-				fileObject.delete();
-			}
-		}
-	}
-
-	public void createFolder(String path) throws COMException {
-		String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
-		PortableDeviceStorageObject storage = getStorage();
-		MTPUtil.createFolder(path, storage, null, lastpartofpath);
-	}
-
-	public ArrayList<String> getAllFilesByName(String path) {
-		ArrayList<String> fileNames = new ArrayList<String>();
-
-		String lastpartofpath = path.substring(path.lastIndexOf("\\") + 1);
-		PortableDeviceStorageObject storage = getStorage();
-
-		PortableDeviceFolderObject folder = MTPUtil.createFolder(path, storage, null, lastpartofpath);
-
-		for (PortableDeviceObject fileObject : folder.getChildObjects()) {
-			if (fileObject != null) {
-				fileNames.add(fileObject.getOriginalFileName());
-			}
-		}
-
-		return fileNames;
-	}
-
-	public PortableDevice[] getDevices() {
-		PortableDeviceManager manager = new PortableDeviceManager();
-		manager.refreshDeviceList();
-		return manager.getDevices();
-	}
-
-	private PortableDeviceStorageObject getStorage() {
-
-		if (device.getRootObjects() != null) {
-
-			for (PortableDeviceObject object : device.getRootObjects()) {
-
-				if (object instanceof PortableDeviceStorageObject) {
-					PortableDeviceStorageObject storage = (PortableDeviceStorageObject) object;
-					return storage;
-				}
-			}
-		}
-		return null;
-	}
-
+    private PortableDeviceStorageObject getStorage() {
+        if (device.getRootObjects() != null) {
+            for (PortableDeviceObject object : device.getRootObjects()) {
+                if (object instanceof PortableDeviceStorageObject) {
+                    PortableDeviceStorageObject storage = (PortableDeviceStorageObject) object;
+                    return storage;
+                }
+            }
+        }
+        return null;
+    }
 }
--- src/util/MTPUtil.java
+++ src/util/MTPUtil.java
@@ -13,45 +13,47 @@ import jmtp.PortableDeviceStorageObject;
 /**
  * @author Philipp Ebert
  * 
- * Various util methods for MTP device communication
+ *         Various util methods for MTP device communication
  */
 public class MTPUtil {
+    private static PortableDeviceFolderObject folderNew;
+    private static PortableDeviceManager manager = new PortableDeviceManager();
 
-    static PortableDeviceFolderObject folderNew;
-
-    public static PortableDeviceFolderObject createFolder(String s, PortableDeviceStorageObject storage, PortableDeviceFolderObject folder,
-            String lastDir) {
-
-        s = s.substring((s.indexOf("\\") + 1), s.length());
-        if (s.indexOf("\\") != -1) {
-            String z = s.substring(0, s.indexOf("\\"));
+    public static PortableDeviceFolderObject createFolder(String path,
+            PortableDeviceStorageObject storage,
+            PortableDeviceFolderObject folder, String lastDir) {
+    	path = path.substring((path.indexOf("\\") + 1), path.length());
+        if (path.indexOf("\\") != -1) {
+            String fileName = path.substring(0, path.indexOf("\\"));
             if (folder == null) {
-                folderNew = (PortableDeviceFolderObject) getChildByName(storage, z);
+                folderNew = (PortableDeviceFolderObject) getChildByName(storage, fileName);
                 if (folderNew == null) {
-                    folderNew = storage.createFolderObject(z);
-                    //LogUtil.debugPrint(LogUtil.LOG_LEVEL_FULL,MTPUtil.class.getSimpleName(),  "Created Root Directory " + z);
+                    folderNew = storage.createFolderObject(fileName);
+                    // LogUtil.debugPrint(LogUtil.LOG_LEVEL_FULL,MTPUtil.class.getSimpleName(),
+                    // "Created Root Directory " + z);
                 }
-                return createFolder(s, storage, folderNew, lastDir);
+                return createFolder(path, storage, folderNew, lastDir);
             } else {
-                folderNew = (PortableDeviceFolderObject) getChildByName(folder, z);
+                folderNew = (PortableDeviceFolderObject) getChildByName(folder, fileName);
                 if (folderNew == null) {
-                    folderNew = folder.createFolderObject(z);
-                    //LogUtil.debugPrint(LogUtil.LOG_LEVEL_FULL, MTPUtil.class.getSimpleName(), "Created Directory " + z);
+                    folderNew = folder.createFolderObject(fileName);
+                    // LogUtil.debugPrint(LogUtil.LOG_LEVEL_FULL,
+                    // MTPUtil.class.getSimpleName(), "Created Directory " + z);
                 }
-                return createFolder(s, storage, folderNew, lastDir);
+                return createFolder(path, storage, folderNew, lastDir);
             }
-        } else {
-            // we have reached the top, try to create the last folder
-            
-            if(folder==null){
-                //no need to do anything
-//              storage.createFolderObject(lastDir);
-                folderNew = (PortableDeviceFolderObject) getChildByName(storage, lastDir); 
-            }
-            else {
+        } else { // we have reached the top, try to create the last folder
+            if (folder == null) {
+                // no need to do anything
+                // storage.createFolderObject(lastDir);
+                folderNew = (PortableDeviceFolderObject) getChildByName(storage,
+                        lastDir);
+            } else {
                 folderNew = getChildByName(folder, lastDir);
                 if (folderNew == null) {
-                    //LogUtil.debugPrint(LogUtil.LOG_LEVEL_FULL, MTPUtil.class.getSimpleName(), "Created Last Directory " + lastDir);
+                    // LogUtil.debugPrint(LogUtil.LOG_LEVEL_FULL,
+                    // MTPUtil.class.getSimpleName(), "Created Last Directory "
+                    // + lastDir);
                     folderNew = folder.createFolderObject(lastDir);
                 }
             }
@@ -59,20 +61,26 @@ public class MTPUtil {
         }
     }
 
-    private static PortableDeviceFolderObject getChildByName(PortableDeviceFolderObject folder, String z) {
+    //private static PortableDeviceFolderObject getChildByName(
+    public static PortableDeviceFolderObject getChildByName(
+            PortableDeviceFolderObject folder, String childName) {
         for (PortableDeviceObject object : folder.getChildObjects()) {
-            if (object.getOriginalFileName().equals(z)) {
-                //LogUtil.debugPrint(LogUtil.LOG_LEVEL_FULL,MTPUtil.class.getSimpleName(),  "Found directory " + z);
+            if (object.getOriginalFileName().equals(childName)) {
+                // LogUtil.debugPrint(LogUtil.LOG_LEVEL_FULL,MTPUtil.class.getSimpleName(),
+                // "Found directory " + z);
                 return (PortableDeviceFolderObject) object;
             }
         }
         return null;
     }
 
-    private static PortableDeviceObject getChildByName(PortableDeviceStorageObject storage, String z) {
+    //private static PortableDeviceObject getChildByName(
+    public static PortableDeviceObject getChildByName(
+            PortableDeviceStorageObject storage, String childName) {
         for (PortableDeviceObject object : storage.getChildObjects()) {
-            if (object.getOriginalFileName().equals(z)) {
-                //LogUtil.debugPrint(LogUtil.LOG_LEVEL_FULL, MTPUtil.class.getSimpleName(),  "Found directory " + z);
+            if (object.getOriginalFileName().equals(childName)) {
+                // LogUtil.debugPrint(LogUtil.LOG_LEVEL_FULL,
+                // MTPUtil.class.getSimpleName(), "Found directory " + z);
                 return object;
             }
         }
@@ -80,16 +88,24 @@ public class MTPUtil {
     }
 
     public static PortableDevice[] getDevices() {
-        PortableDeviceManager manager = new PortableDeviceManager();
         manager.refreshDeviceList();
         return manager.getDevices();
     }
+    
+    public static ArrayList<PortableDeviceStorageObject> getDeviceStorages(PortableDevice device) {
+        manager.refreshDeviceList();
+        ArrayList<PortableDeviceStorageObject> storageList = new ArrayList<>();
+        for (PortableDeviceObject object : device.getRootObjects()) {
+            if (!(object instanceof PortableDeviceStorageObject)) {
+                continue;
+            }
+            storageList.add((PortableDeviceStorageObject) object);
+        }
+        return storageList;
+    }
 
     public static void printDevices() {
-        PortableDeviceManager manager = new PortableDeviceManager();
-
         manager.refreshDeviceList();
-
         for (PortableDevice device : manager.getDevices()) {
             device.open();
             System.out.println("MTPUtil - Devices - " + device.getModel());
@@ -98,28 +114,26 @@ public class MTPUtil {
     }
 
     public static void printAll() {
-        PortableDeviceManager manager = new PortableDeviceManager();
-
         manager.refreshDeviceList();
-
         for (PortableDevice device : manager.getDevices()) {
             device.open();
-
             System.out.println("MTPUtil Device - " + device.getModel());
-            
             if (device.getRootObjects().length == 0) {
-                System.out.println("Could not access " + device.getModel() +
-                        "'s storage. Make sure it has been unlocked once since connecting it.");
+                System.out.println("Could not access " + device.getModel()
+                        + "'s storage. Make sure it has been unlocked once since connecting it.");
             }
             for (PortableDeviceObject object : device.getRootObjects()) {
                 if (!(object instanceof PortableDeviceStorageObject)) {
                     continue;
                 }
+                
+                
                 PortableDeviceStorageObject storage = (PortableDeviceStorageObject) object;
                 System.out.println("New storage! Traversing...");
                 System.out.println("Name: " + storage.getName());
 
-                for (PortableDeviceObject child : ((PortableDeviceStorageObject) storage).getChildObjects()) {
+                for (PortableDeviceObject child : ((PortableDeviceStorageObject) storage)
+                        .getChildObjects()) {
                     System.out.println("MTPUtil Dir - " + child.getName());
                 }
             }
@@ -133,18 +147,13 @@ public class MTPUtil {
      */
     public static ArrayList<MTPDeviceInfo> getDeviceModels() {
         ArrayList<MTPDeviceInfo> models = new ArrayList<MTPDeviceInfo>();
-        
-        PortableDeviceManager manager = new PortableDeviceManager();
         manager.refreshDeviceList();
-
         for (PortableDevice device : manager.getDevices()) {
             device.open();
-            models.add(new MTPDeviceInfo(device.getModel(), device.getSerialNumber()));
+            models.add(new MTPDeviceInfo(device.getModel(),
+                    device.getSerialNumber()));
             device.close();
         }
-        
         return models;
-
     }
-
 }
