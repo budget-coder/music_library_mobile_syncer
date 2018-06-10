f303e2ff7ed33ae40c42c04d8e05d6f46faf231f
fabervae <mesebii@hotmail.com>
2018-06-11 00:32:58 +0200

    Removed instance of File dstFolder. Not tested.

--- src/filesystem/PCDeviceStrategy.java
+++ src/filesystem/PCDeviceStrategy.java
@@ -1,6 +1,5 @@
 package filesystem;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,25 +10,27 @@ import framework.DeviceStrategy;
 import framework.FileWrapper;
 
 public class PCDeviceStrategy implements DeviceStrategy {
-	private final File dstFolder;
+	//private final File dstFolder;
 	private final PCFile dstFolderPC;
 
-	public PCDeviceStrategy(File dstFolder) {
-		this.dstFolder = dstFolder;
+	public PCDeviceStrategy(String dstFolder) {
+		//this.dstFolder = new File(dstFolder);
 		dstFolderPC = new PCFile(dstFolder);
 	}
 	
 	@Override
 	public boolean isDstADirectory() {
-		return dstFolder.isDirectory();
+		//return dstFolder.isDirectory();
+		return dstFolderPC.isDirectory();
 	}
 	
 	@Override
 	public FileWrapper[] listDstFiles() {
-		final File[] dstFolderList = dstFolder.listFiles();
+		//final File[] dstFolderList = dstFolder.listFiles();
+		final FileWrapper[] dstFolderList = dstFolderPC.listFiles();
 		FileWrapper[] returnList = new FileWrapper[dstFolderList.length];
-		for (int i = 0; i < dstFolderList.length; i++) {
-			returnList[i] = new PCFile(dstFolderList[i]);
+		for (int i = 0; i < dstFolderList.length; ++i) {
+			returnList[i] = new PCFile(dstFolderList[i].getAbsolutePath());
 		}
 		return returnList;
 	}
@@ -46,7 +47,8 @@ public class PCDeviceStrategy implements DeviceStrategy {
 
 	@Override
 	public void copyMusicToDst(FileWrapper newMusic) throws IOException {
-		Path targetPath = Paths.get(dstFolder.getAbsolutePath()).resolve(newMusic.getName());
+		//Path targetPath = Paths.get(dstFolder.getAbsolutePath()).resolve(newMusic.getName());
+		Path targetPath = Paths.get(dstFolderPC.getAbsolutePath()).resolve(newMusic.getName());
 		Files.copy(Paths.get(newMusic.getAbsolutePath()), targetPath,
 					StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
 	}
