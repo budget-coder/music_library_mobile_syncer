40841379f6a0ff16dfa2712ef03c471ac894ee7b
fabervae <mesebii@hotmail.com>
2018-06-11 00:28:23 +0200

    Missing listFiles() and verification of other methods.

--- src/filesystem/MTPFile.java
+++ src/filesystem/MTPFile.java
@@ -1,39 +1,71 @@
 package filesystem;
 
-import java.io.IOException;
-
 import framework.FileWrapper;
+import jmtp.PortableDeviceFolderObject;
+import jmtp.PortableDeviceObject;
+import jmtp.PortableDeviceStorageObject;
+import util.MTPUtil;
 
 public class MTPFile implements FileWrapper {
+	private PortableDeviceObject file;
+	
+	public MTPFile(PortableDeviceStorageObject storage, String pathToFile) {
+		file = MTPUtil.getChildByName(storage, pathToFile);
+	}
 
 	@Override
 	public boolean isDirectory() {
-		// TODO Auto-generated method stub
-		return false;
+		if ((PortableDeviceFolderObject) file != null) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return file.getName();
 	}
 
 	@Override
 	public boolean deleteFile() {
-		// TODO Auto-generated method stub
+		if (file.canDelete()) {
+			file.delete();
+			return true;
+		}
 		return false;
 	}
 
-	@Override
-	public int getUniqueHash() throws IOException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+//	@Override
+//	public int getUniqueHash() throws IOException {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
 
 	@Override
 	public boolean doesFileExist() {
+		return file != null ? true : false;
+	}
+
+	@Override
+	public String getAbsolutePath() {
 		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException(getClass().getName() + " : Not implemented yet.");
+	}
+
+	@Override
+	public FileWrapper[] listFiles() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException(getClass().getName() + " : Not implemented yet.");
+		/*
+		PortableDeviceObject[] deviceObjList = ((PortableDeviceFolderObject) file).getChildObjects();
+		FileWrapper[] fileList = new FileWrapper[deviceObjList.length];
+		for (int i = 0; i < deviceObjList.length; ++i) {
+			// TODO Implement ths correctly using the code from TestEnvironment, line 61
+			//fileList[i] = new MTPFile(deviceObjList[i].)
+		}
+		return fileList;
+		*/
 	}
 
 }
