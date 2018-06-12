e61ca6d8ad9297387ee9c777a88c83344b0a3fb2
fabervae <mesebii@hotmail.com>
2018-06-12 19:51:02 +0200

    Copy of strategy and state pattern before completely revising them.

--- src/framework/StateDeviceStrategy.java
+++ src/framework/StateDeviceStrategy.java
@@ -1,5 +1,13 @@
 package framework;
 
 public interface StateDeviceStrategy extends DeviceStrategy {
-	public void setToPCOrDevice(boolean isPC);
+	//public void setToPCOrDevice(boolean isDevice);
+
+	public void setSrcAsMTPDevice(boolean option);
+
+	public void setDstAsMTPDevice(boolean option);
+
+	FileWrapper getSrcFileInstance(String path);
+
+	FileWrapper getDstFileInstance(String path);
 }
--- src/main/SwitchBetweenDevicesStrategy.java
+++ src/main/SwitchBetweenDevicesStrategy.java
@@ -7,43 +7,71 @@ import framework.FileWrapper;
 import framework.StateDeviceStrategy;
 
 public class SwitchBetweenDevicesStrategy implements StateDeviceStrategy {
-	private DeviceStrategy mtpStrategy;
-	private DeviceStrategy pcStrategy;
-	private DeviceStrategy currentState;
+	private final DeviceStrategy mtpStrategy;
+	private final DeviceStrategy pcStrategy;
+	private DeviceStrategy currentSrcState;
+	private DeviceStrategy currentDstState;
 	
-	public SwitchBetweenDevicesStrategy(DeviceStrategy mtpStrategy,
-			DeviceStrategy pcStrategy) {
+	public SwitchBetweenDevicesStrategy(final DeviceStrategy mtpStrategy,
+			final DeviceStrategy pcStrategy) {
 		this.mtpStrategy = mtpStrategy;
 		this.pcStrategy = pcStrategy;
 	}
 	
 	@Override
-	public void setToPCOrDevice(boolean isDevice) {
-		currentState = (isDevice ? mtpStrategy : pcStrategy); // Same as if-then-else
+	public void setSrcAsMTPDevice(boolean isDevice) {
+		currentSrcState = (isDevice ? mtpStrategy : pcStrategy);
 	}
 
 	@Override
+	public void setDstAsMTPDevice(boolean isDevice) {
+		currentDstState = (isDevice ? mtpStrategy : pcStrategy);
+	}
+	
+	/*
+	@Override
+	public void setToPCOrDevice(boolean isDevice) {
+		currentState = (isDevice ? mtpStrategy : pcStrategy); // Same as if-then-else
+	}
+	*/
+
+	@Override
 	public FileWrapper[] listDstFiles() {
-		return currentState.listDstFiles();
+		return currentDstState.listDstFiles();
 	}
 
 	@Override
 	public void copyMusicToDst(FileWrapper newMusic) throws IOException {
-		currentState.copyMusicToDst(newMusic);
+		// Iff. source and destination are on the PC, then use pcStrategy. Otherwise, use mtpStrategy.
+		if (currentSrcState.equals(currentDstState) && currentSrcState.equals(pcStrategy)) {
+			pcStrategy.copyMusicToDst(newMusic);
+		}
 	}
 
 	@Override
 	public boolean isDstADirectory() {
-		return currentState.isDstADirectory();
+		return currentDstState.isDstADirectory();
 	}
 
 	@Override
 	public FileWrapper getDstFolder() {
-		return currentState.getDstFolder();
+		return currentDstState.getDstFolder();
+	}
+
+	@Override
+	public FileWrapper getSrcFileInstance(String path) {
+		return currentSrcState.getFileInstance(path);
+	}
+	
+	@Override
+	public FileWrapper getDstFileInstance(String path) {
+		return currentDstState.getFileInstance(path);
 	}
 
 	@Override
 	public FileWrapper getFileInstance(String path) {
-		return currentState.getFileInstance(path);
+		// TODO State pattern seems hard to make use of here other than when
+		// using the copy function.
+		throw new UnsupportedOperationException(getClass().getName() + ": getFileInstance not implemented");
 	}
 }
