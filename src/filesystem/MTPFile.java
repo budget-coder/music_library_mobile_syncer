88c1b1a90e0778d238bcebecd9368b2ef226409d
fabervae <mesebii@hotmail.com>
2018-06-12 18:53:26 +0200

    Constructor now looks up file/folder recursively. See TODO for how to improve

--- src/filesystem/MTPFile.java
+++ src/filesystem/MTPFile.java
@@ -7,15 +7,47 @@ import jmtp.PortableDeviceStorageObject;
 import util.MTPUtil;
 
 public class MTPFile implements FileWrapper {
-	private PortableDeviceObject file;
+	private final PortableDeviceObject file;
 	
+	/**
+	 * Constructs a valid MTPFile for a valid storage and file path.
+	 * 
+	 * @param storage
+	 *            - the storage object. If no storage is known at the time, then
+	 *            passing "null" will construct an invalid MTPFile. This means that
+	 *            all methods will do nothing or return default values (such as the
+	 *            empty string).
+	 * @param pathToFile
+	 *            - the path to the folder or file.
+	 * 
+	 */
 	public MTPFile(PortableDeviceStorageObject storage, String pathToFile) {
-		file = MTPUtil.getChildByName(storage, pathToFile);
+		// TODO Improve with Null Pattern for storage (!!!that and folderObject in MTPUtil!!!)
+		if (storage != null) {
+			file = MTPUtil.getChildFileByNameRecursively(storage, pathToFile);
+		} else {
+			file = new NullPortableDeviceObject();
+		}
+	}
+	
+	/**
+	 * Constructs a valid MTPFile for a valid folder and file path. Behaves
+	 * similarly to the referred constructor.
+	 * 
+	 * @see #MTPFile(PortableDeviceStorageObject, String)
+	 */
+	public MTPFile(PortableDeviceFolderObject parentFolder, String pathToFile) {
+		// TODO Improve with Null Pattern for storage (!!!that and folderObject in MTPUtil!!!)
+		if (parentFolder == null || parentFolder instanceof NullPortableDeviceFolderObject) {
+			file = new NullPortableDeviceObject();
+		} else {
+			file = MTPUtil.getChildFileByNameRecursively(parentFolder, pathToFile);
+		}
 	}
 
 	@Override
 	public boolean isDirectory() {
-		if ((PortableDeviceFolderObject) file != null) {
+		if (file instanceof PortableDeviceFolderObject) {
 			return true;
 		} else {
 			return false;
@@ -55,7 +87,7 @@ public class MTPFile implements FileWrapper {
 
 	@Override
 	public FileWrapper[] listFiles() {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub. RETURN NULL JUST LIKE java.io.File if path is not directory.
 		throw new UnsupportedOperationException(getClass().getName() + " : Not implemented yet.");
 		/*
 		PortableDeviceObject[] deviceObjList = ((PortableDeviceFolderObject) file).getChildObjects();
