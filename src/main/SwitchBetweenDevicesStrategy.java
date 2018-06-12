637daca5942dbac49699ce847a26c2f13f4d795c
fabervae <mesebii@hotmail.com>
2018-06-13 01:15:40 +0200

    SwitchBetweenStrats should be fixed (i.e. sane) and ready for testing

--- src/main/SwitchBetweenDevicesStrategy.java
+++ src/main/SwitchBetweenDevicesStrategy.java
@@ -2,11 +2,12 @@ package main;
 
 import java.io.IOException;
 
+import filesystem.NullDeviceStrategy;
 import framework.DeviceStrategy;
 import framework.FileWrapper;
 import framework.StateDeviceStrategy;
 
-public class SwitchBetweenDevicesStrategy implements StateDeviceStrategy {
+public class SwitchBetweenDevicesStrategy extends NullDeviceStrategy implements StateDeviceStrategy {
 	private final DeviceStrategy mtpStrategy;
 	private final DeviceStrategy pcStrategy;
 	private DeviceStrategy currentSrcState;
@@ -17,7 +18,27 @@ public class SwitchBetweenDevicesStrategy implements StateDeviceStrategy {
 		this.mtpStrategy = mtpStrategy;
 		this.pcStrategy = pcStrategy;
 	}
+
+	@Override
+	public void copyMusicToDst(FileWrapper newMusic) throws IOException {
+		// Iff. source and destination are on the PC, then use pcStrategy. Otherwise, use mtpStrategy.
+		if (currentSrcState.equals(currentDstState) && currentSrcState.equals(pcStrategy)) {
+			pcStrategy.copyMusicToDst(newMusic);
+		} else {
+			mtpStrategy.copyMusicToDst(newMusic);
+		}
+	}
 	
+	@Override
+	public FileWrapper getSrcFolder() {
+		return currentSrcState.getFolder();
+	}
+	
+	@Override
+	public FileWrapper getDstFolder() {
+		return currentDstState.getFolder();
+	}
+
 	@Override
 	public void setSrcAsMTPDevice(boolean isDevice) {
 		currentSrcState = (isDevice ? mtpStrategy : pcStrategy);
@@ -26,52 +47,5 @@ public class SwitchBetweenDevicesStrategy implements StateDeviceStrategy {
 	@Override
 	public void setDstAsMTPDevice(boolean isDevice) {
 		currentDstState = (isDevice ? mtpStrategy : pcStrategy);
-	}
-	
-	/*
-	@Override
-	public void setToPCOrDevice(boolean isDevice) {
-		currentState = (isDevice ? mtpStrategy : pcStrategy); // Same as if-then-else
-	}
-	*/
-
-	@Override
-	public FileWrapper[] listDstFiles() {
-		return currentDstState.listDstFiles();
-	}
-
-	@Override
-	public void copyMusicToDst(FileWrapper newMusic) throws IOException {
-		// Iff. source and destination are on the PC, then use pcStrategy. Otherwise, use mtpStrategy.
-		if (currentSrcState.equals(currentDstState) && currentSrcState.equals(pcStrategy)) {
-			pcStrategy.copyMusicToDst(newMusic);
-		}
-	}
-
-	@Override
-	public boolean isDstADirectory() {
-		return currentDstState.isDstADirectory();
-	}
-
-	@Override
-	public FileWrapper getDstFolder() {
-		return currentDstState.getDstFolder();
-	}
-
-	@Override
-	public FileWrapper getSrcFileInstance(String path) {
-		return currentSrcState.getFileInstance(path);
-	}
-	
-	@Override
-	public FileWrapper getDstFileInstance(String path) {
-		return currentDstState.getFileInstance(path);
-	}
-
-	@Override
-	public FileWrapper getFileInstance(String path) {
-		// TODO State pattern seems hard to make use of here other than when
-		// using the copy function.
-		throw new UnsupportedOperationException(getClass().getName() + ": getFileInstance not implemented");
 	}
 }
