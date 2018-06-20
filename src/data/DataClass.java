34374d2f6cb0288c6bec5ba7cd774af19dfa0238
fabervae <mesebii@hotmail.com>
2018-06-20 19:52:23 +0200

    Contains a unique UUID error string which can be used when returning error on tag data.

--- src/data/DataClass.java
+++ src/data/DataClass.java
@@ -1,10 +1,11 @@
 package data;
 import java.awt.Color;
+import java.util.UUID;
 
-public class DataClass {
+public class DataClass {	
     public static final Color INFO_COLOR        = Color.BLUE;
     public static final Color NEW_MUSIC_COLOR   = new Color(0, 220, 0);
     public static final Color DEL_MUSIC_COLOR   = Color.ORANGE;
     public static final Color ERROR_COLOR       = Color.RED;
-
+    public static final String ERROR_STRING		= UUID.randomUUID().toString();
 }
