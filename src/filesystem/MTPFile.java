fea3a5d61abe5fcae352049f9fec70a2d505c51f
fabervae <mesebii@hotmail.com>
2018-07-19 16:52:25 +0200

    Implemented a lot of tagging methods. BUG: PDOIW32::changeXValue(...) throws UOE...

--- src/filesystem/MTPFile.java
+++ src/filesystem/MTPFile.java
@@ -1,7 +1,9 @@
 package filesystem;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Calendar;
 
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.images.Artwork;
@@ -17,6 +19,7 @@ import util.MTPUtil;
 public class MTPFile implements FileWrapper {
 	private final PortableDeviceObject file;
 	private final PortableDeviceAudioObject audioFile;
+	private final String pathToFile; 
 	
 	/**
 	 * Constructs a valid MTPFile for a valid storage and file path.
@@ -31,6 +34,7 @@ public class MTPFile implements FileWrapper {
 	 * 
 	 */
 	public MTPFile(PortableDeviceStorageObject storage, String pathToFile) {
+		this.pathToFile = pathToFile;
 		// TODO Improve with Null Pattern for storage (!!!that and folderObject in MTPUtil!!!)
 		if (storage != null) {
 			file = MTPUtil.getChildFileByNameRecursively(storage, pathToFile);
@@ -52,15 +56,34 @@ public class MTPFile implements FileWrapper {
 	 * @see #MTPFile(PortableDeviceStorageObject, String)
 	 */
 	public MTPFile(PortableDeviceFolderObject parentFolder, String pathToFile) {
+		this.pathToFile = pathToFile;
 		// TODO Improve with Null Pattern for storage (!!!that and folderObject in MTPUtil!!!)
 		if (parentFolder == null || parentFolder instanceof NullPortableDeviceFolderObject) {
 			file = new NullPortableDeviceObject();
+			System.out.println("MTP: path + " + pathToFile + " does not exist!");
 		} else {
 			file = MTPUtil.getChildFileByNameRecursively(parentFolder, pathToFile);
+			System.out.println("MTP: Constructed file of path " + pathToFile + " Path is folder? " + (file instanceof PortableDeviceFolderObject));
 		}
 		if (file instanceof PortableDeviceAudioObject) {
 			audioFile = (PortableDeviceAudioObject) file;
 		} else {
+			audioFile = null;
+		}
+	}
+	
+	/**
+	 * Private constructor for directly creating <b>known</b> MTP files.
+	 * 
+	 * @see #MTPFile(PortableDeviceStorageObject, String)
+	 */
+	private MTPFile(PortableDeviceObject file, String pathToFile) {
+		this.file = file;
+		this.pathToFile = pathToFile;
+		if (file instanceof PortableDeviceAudioObject) {
+			audioFile = (PortableDeviceAudioObject) file;
+		} else {
+			// TODO improve with null pattern here as well
 			audioFile = null;
 		}
 	}
@@ -83,7 +106,7 @@ public class MTPFile implements FileWrapper {
 	public boolean deleteFile() {
 		if (file.canDelete()) {
 			file.delete();
-			return true;
+			return true;	
 		}
 		return false;
 	}
@@ -101,28 +124,30 @@ public class MTPFile implements FileWrapper {
 
 	@Override
 	public String getAbsolutePath() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException(getClass().getName() + " : Not implemented yet.");
+		return pathToFile;
 	}
 
 	@Override
 	public FileWrapper[] listFiles() {
-		// TODO Auto-generated method stub. RETURN NULL JUST LIKE java.io.File if path is not directory.
-		throw new UnsupportedOperationException(getClass().getName() + " : Not implemented yet.");
-		/*
+		if (!isDirectory()) {
+			return null;
+		}
 		PortableDeviceObject[] deviceObjList = ((PortableDeviceFolderObject) file).getChildObjects();
 		FileWrapper[] fileList = new FileWrapper[deviceObjList.length];
 		for (int i = 0; i < deviceObjList.length; ++i) {
-			// TODO Implement ths correctly using the code from TestEnvironment, line 61
-			//fileList[i] = new MTPFile(deviceObjList[i].)
+			fileList[i] = new MTPFile(deviceObjList[i],
+					pathToFile + File.separator + deviceObjList[i].getOriginalFileName());
 		}
 		return fileList;
-		*/
 	}
 
 	@Override
 	public String getDuration() {
-		return audioFile.getDuration().toString();
+		String duration = audioFile.getDuration().toString();
+		// TODO jaudiotagger solution for duration did not work, so it has been changed
+		// to getting it from AudioHeader, which only supports duration down to seconds.
+		// Therefore, we cut off the milliseconds here with "-3".
+		return duration.substring(0, duration.length()-3);
 	}
 
 	@Override
@@ -156,8 +181,44 @@ public class MTPFile implements FileWrapper {
 
 	@Override
 	public void changeTag(FieldKey fieldKey, String tagValueSrc) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Changing tag " + fieldKey + " to \"" + tagValueSrc + "\"");
+		switch (fieldKey) {
+		case TITLE:
+			audioFile.setTitle(tagValueSrc);
+			break;
+		case ARTIST:
+			audioFile.setArtist(tagValueSrc);
+			break;
+		case ALBUM_ARTIST:
+			audioFile.setAlbumArtist(tagValueSrc);
+			break;
+		case ALBUM:
+			audioFile.setAlbum(tagValueSrc);
+			break;
+		case YEAR:
+			// This is a very roundabout way of changing the year because Date's methods are
+			// more or less deprecated.
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(audioFile.getReleaseDate());
+			cal.set(Calendar.YEAR, Integer.parseInt(tagValueSrc));
+			audioFile.setReleaseDate(cal.getTime());
+			break;
+		case COMPOSER:
+			audioFile.setComposer(tagValueSrc);
+			break;
+		case DISC_NO:
+			throw new UnsupportedOperationException(getClass().getName() + ": DISC_NO not implemented yet (because jmtp is incomplete)");
+			// REMEMBER BREAK
+		case GENRE:
+			audioFile.setGenre(tagValueSrc);
+			break;
+		case TRACK:
+			audioFile.setTrackNumber(Integer.parseInt(tagValueSrc));
+			break;
+		default:
+			System.err.println("FATAL: Unknown tag " + fieldKey + " from mtp is requested");
+			break;
+		}
 	}
 
 	@Override
@@ -166,12 +227,11 @@ public class MTPFile implements FileWrapper {
 
 	@Override
 	public Artwork getAlbumArt() {
-		throw new UnsupportedOperationException(getClass().getName() + ": Not implemented yet");
+		throw new UnsupportedOperationException(getClass().getName() + ": getAlbumArt() not implemented yet");
 	}
 
 	@Override
 	public void changeAlbumArt(Artwork newArt) {
-		throw new UnsupportedOperationException(getClass().getName() + ": Not implemented yet");
+		throw new UnsupportedOperationException(getClass().getName() + ": changeAlbumArt() not implemented yet");
 	}
-
 }
